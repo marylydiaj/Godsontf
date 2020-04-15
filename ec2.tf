92ae1e06ffcdf26ac8be2ad9703d849566a28f31
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-15 17:25:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -61,6 +61,30 @@ resource "aws_security_group" "sg" {
    protocol    = "icmp"
    cidr_blocks = ["0.0.0.0/0"]
  }
+  egress {
+   from_port   = 50000
+   to_port     = 50000
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+ ingress {
+   from_port   = 50000
+   to_port     = 50000
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+   egress {
+   from_port   = 8080
+   to_port     = 8080
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+ ingress {
+   from_port   = 8080
+   to_port     = 8080
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
  ingress {
        from_port   = 80
        to_port     = 80
