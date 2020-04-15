2c99c0e79776db341cfe7cf898ea8595e1707902
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-15 12:22:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,3 @@
  sudo yum install git -y
  sudo yum update -y
  sudo yum install python-django -y
- git clone https://github.com/GodsonSibreyan/project-python-django-webapp.git
- cd project-python-django-webapp/
- sudo python manage.py migrate
- sudo python manage.py runserver 0.0.0.0:8000
