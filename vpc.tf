6b972fadc2aa4b23fc0f98464862dc4dff07601a
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-17 12:47:35 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -44,11 +44,6 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-resource "aws_route" "public_route" {
-  route_table_id  = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-}
-
 resource "aws_route_table_association" "public_subnet_association" {
    subnet_id = aws_subnet.public_subnet.id
    route_table_id = aws_route_table.public_route_table.id
