528857b6486f96a396d78a395fc91686148ad452
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-17 12:38:04 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,20 +20,6 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-#### Route table ####
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc_name.id
-  tags = {
-    Name = "public route table"
-  }
-}
-
-resource "aws_route_table_association" "public_subnet_association" {
-   subnet_id = aws_subnet.public_subnet.id
-   route_table_id = aws_route_table.public_route_table.id
-}
-
 ###### Internet Gateway #####
 
 resource "aws_internet_gateway" "gw" {
@@ -47,6 +33,25 @@ resource "aws_route" "internet_access" {
  route_table_id         = aws_vpc.vpc_name.main_route_table_id
  destination_cidr_block = "0.0.0.0/0"
  gateway_id             = aws_internet_gateway.gw.id
+}
+
+#### Route table ####
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc_name.id
+  tags = {
+    Name = "public route table"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id  = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+   subnet_id = aws_subnet.public_subnet.id
+   route_table_id = aws_route_table.public_route_table.id
 }
 
 ##### Elastic IP #####
