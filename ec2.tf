bb7007ad8f86eec38022941b8bfc8ed5acba9312
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-17 12:01:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -51,79 +51,79 @@ resource "aws_security_group" "sg" {
    from_port   = 22
    to_port     = 22
    protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
  egress {
    from_port   = 0
    to_port     = 0
    protocol    = "-1"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
  ingress {
    from_port   = 8
    to_port     = 0
    protocol    = "icmp"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
   egress {
    from_port   = 50000
    to_port     = 50000
    protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
  ingress {
    from_port   = 50000
    to_port     = 50000
    protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
    egress {
    from_port   = 8080
    to_port     = 8080
    protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
  ingress {
    from_port   = 8080
    to_port     = 8080
    protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
+   cidr_blocks = ["11.0.1.0/22"]
  }
  ingress {
        from_port   = 80
        to_port     = 80
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
+       cidr_blocks = ["11.0.1.0/22"]
  }
  ingress {
        from_port   = 443
        to_port     = 443
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
+       cidr_blocks = ["11.0.1.0/22"]
    }
  ingress {
        from_port   = 8000
        to_port     = 8000
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
+       cidr_blocks = ["11.0.1.0/22"]
   }
    egress {
        from_port   = 80
        to_port     = 80
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
+       cidr_blocks = ["11.0.1.0/22"]
    }
    egress {
        from_port   = 443
        to_port     = 443
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
+       cidr_blocks = ["11.0.1.0/22"]
    }
    egress {
        from_port   = 8000
        to_port     = 8000
        protocol    = "tcp"
-       cidr_blocks = ["0.0.0.0/0"]
+       cidr_blocks = ["11.0.1.0/22"]
    }
  tags = {
    Name        = "gods-sg"
