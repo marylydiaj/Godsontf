e945169ece045857e543e778fe25165621c2f5fc
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-20 08:54:25 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,12 +3,8 @@
  sudo yum install epel-release -y
  sudo yum install git -y
  sudo yum update -y
- sudo yum install screen -y
  sudo yum install python-django -y
- sudo yum -y install mariadb-server
- sudo systemctl start mariadb
- sudo systemctl enable mariadb
- sudo sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
- sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDlvu4zNd+Ng4K5twKW3iaxvxXXD4pZ8iehQ8h+fDoDdEQIjV6pTfmTmFdYY1Ilt54ETvpvGSZkM3aPbqBX1HEmt3sc/JF8EjdQ63L0phhGnjulLeUIGCydNANZSedTfmcQ+llbaFIrhYNiKMOwkAARj8Sb3E1Y6ZsoUCGekPkDw8s1OlJEhIudxKT3Y7SsvCuP8aWgposC4DGBbBIIq+UipqBI0l6kOFB+fp8hPDY3x4AnrpxqeAgXKpTPYGP53z3vJF25l2K4s3+53mfy+c5c2NcoGxbE0hB1E5fWyaBun9vpRwtwGYBcmH+s0dDIWxR5P+5TsgJl7eO+kSLvDdPp jenkins@jenkins-cloud.novalocal" > /home/ec2-user/.ssh/authorized_keys
- chmod 700 .ssh
- chmod 640 .ssh/authorized_keys
+ git clone https://github.com/GodsonSibreyan/project-python-django-webapp.git
+ cd project-python-django-webapp/
+ sudo python manage.py migrate
+ sudo python manage.py runserver 0.0.0.0:8000
