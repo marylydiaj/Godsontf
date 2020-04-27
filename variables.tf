9aba42c85269862348f7141842907462a18baa04
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-27 16:22:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "aws_access_key" {
-    default = ""
+variable "active_key" {
+    default = "user"
 }
-variable "aws_secret_key" {
-    default = ""
+variable "security_key" {
+    default = "password"
 }
 variable "aws_region" {
     default = "us-east-1"
