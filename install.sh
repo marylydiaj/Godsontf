6fd61ae403702fbef5847410a000ac0f717c7ef8
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-28 11:26:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo amazon-linux-extras install epel -y
 sudo yum install epel-release -y
 sudo yum install git -y
 sudo yum update -y
+sudo yum install ufw -y
 sudo yum install python-django -y
 sudo yum groupinstall 'Development Tools' -y
 sudo wget http://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
