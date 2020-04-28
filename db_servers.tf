88becbf9827341891f3b4ac0c53d1d02d03e30ef
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-04-28 19:44:49 +0530

    Update db_servers.tf

--- db_servers.tf
+++ db_servers.tf
@@ -4,47 +4,68 @@ resource "aws_security_group" "dbsg" {
     vpc_id = aws_vpc.customvpc.id
 
     ingress {
-        from_port = 3306
-        to_port = 3306
-        protocol = "tcp"
-        security_groups = [aws_security_group.websg.id]
-    }
-	egress {
-        from_port = 3306
-        to_port = 3306
-        protocol = "tcp"
-        security_groups = [aws_security_group.websg.id]
-    }
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.vpc_cidr]
-    }
-    ingress {
-        from_port = -1
-        to_port = -1
-        protocol = "icmp"
-        cidr_blocks = [var.vpc_cidr]
-    }
-
-    egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    tags = {
-        Name = "DBServerSG"
-        Terraform = true
-    }
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "TCP"
+    cidr_blocks = ["10.0.2.0/24"]
+  }
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.2.0/24"]
+  }
+  tags = {
+    Name = "sg"
+  }
 }
 
 resource "aws_instance" "db1" {
