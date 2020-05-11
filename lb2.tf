28ae75a1bad192bf25bc62bd63feca06dc55eee1
GodsonSibreyan <61176648+GodsonSibreyan@users.noreply.github.com>
2020-05-11 09:26:25 +0530

    Add files via upload

---
+++ lb2.tf
@@ -0,0 +1,139 @@
+resource "aws_launch_configuration" "LC" {
+  image_id = var.image
+  instance_type = var.instance_type
+  security_groups = [ aws_security_group.LCSG.id ]
+  key_name = var.key
+  associate_public_ip_address = true
+  user_data = data.template_file.LC.rendered
+  root_block_device {
+       volume_type           = "gp2"
+       volume_size           = "10"
+       delete_on_termination = "true"
+    }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+data "template_file" "LC" {
+  template = file("install.sh")
+}
+
+resource "aws_autoscaling_group" "ASG" {
+  launch_configuration = aws_launch_configuration.LC.id
+  vpc_zone_identifier = [ aws_subnet.Publicsubnet1.id,aws_subnet.Publicsubnet2.id ]
+  min_size = 2
+  max_size = 3
+  tags = [
+{
+    key = "Name"
+    value = "autoscale"
+    propagate_at_launch = true
+  }
+]
+}
+
+resource "aws_security_group" "LCSG" {
+  name        = "launch-configuration-sg"
+  description = "Used for autoscale group"
+  vpc_id      = aws_vpc.MyVPC.id
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "LBSG" {
+  name = "load-balancer-SG"
+  vpc_id      = aws_vpc.MyVPC.id
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port = 8000
+    to_port = 8000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "alb" {  
+  name            = "alb"  
+  subnets         = [ aws_subnet.Publicsubnet1.id, aws_subnet.Publicsubnet2.id ]
+  security_groups = [ aws_security_group.LBSG.id ]
+  internal        = false 
+  idle_timeout    = 60   
+  tags = {    
+    Name    = "alb"    
+  }   
+}
+
+resource "aws_lb_target_group" "alb_target_group" {  
+  name     = "alb-target-group"  
+  port     = "8000"  
+  protocol = "HTTP"  
+  vpc_id   = aws_vpc.MyVPC.id
+  tags = {    
+    name = "alb_target_group"    
+  }   
+  stickiness {    
+    type            = "lb_cookie"    
+    cookie_duration = 1800    
+    enabled         = true 
+  }   
+  health_check {    
+    healthy_threshold   = 3    
+    unhealthy_threshold = 10    
+    timeout             = 5    
+    interval            = 10    
+    path                = "/"    
+    port                = 8000
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tga" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = ["aws_instance.Appserver1.id", "aws_instance.Appserver2.id"]
+  port             = 8000
+}
+
+resource "aws_autoscaling_attachment" "alb_autoscale" {
+  alb_target_group_arn   = aws_lb_target_group.alb_target_group.arn
+  autoscaling_group_name = aws_autoscaling_group.ASG.id
+}
+
+resource "aws_lb_listener" "alb_listener" {  
+  load_balancer_arn = aws_lb.alb.arn  
+  port              = 8000 
+  protocol          = "HTTP"
+  
+  default_action {    
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    type             = "forward"  
+  }
+}
